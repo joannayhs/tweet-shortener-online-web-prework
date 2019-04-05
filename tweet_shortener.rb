421ebbd75189144cf0ae1cf3d1a374de6bc60136
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-05 18:29:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+
+ def dictionary
+ words_to_be_substituted = { "hello" => "hi", "to" => "2", "two" => "2", "too" =>  "2", "for" => "4","For" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+end
+ 
+ def word_substituter(string) 
+   tweet = string.split
+   tweet.each do |word|
+     dictionary.map do |key,value|
+       if word == key
+         word.replace value
+       end
+   end
+ end
+  tweet.join(" ")
+ end 
+
+def bulk_tweet_shortener(array)
+  array.collect{|phrase| puts word_substituter(phrase)}
+end 
+
+def selective_tweet_shortener(string)
+if string.length >= 140
+  puts word_substituter(string)
+else
+  puts string
+end
+end
